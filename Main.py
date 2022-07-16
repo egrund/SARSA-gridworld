c87b157c3da41595a2713263d9442671ddc0f401
egrund <82370915+egrund@users.noreply.github.com>
2022-07-16 11:21:31 +0200

    Created todo.txt

--- Main.py
+++ Main.py
@@ -3,19 +3,19 @@ import numpy as np
 import SARSAn
 import Grid
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-# create world
-world = Grid.Gridworld()
+    # create world
+    world = Grid.Gridworld()
 
-# create n-step SARSA Agent
-player = SARSAn.SARSAn(gridworld=world,n=10,epsilon=0.5,gamma = 0.99,alpha = 0.3,visualize_policy = False, visualize_grid = True)
+    # create n-step SARSA Agent
+    player = SARSAn.SARSAn(gridworld=world,n=10,epsilon=0.5,gamma = 0.99,alpha = 0.3,visualize_policy = False, visualize_grid = True)
 
-# create Monte Carlo without Exploring Starts Agent
-# player = SARSAn(gridworld=world,n = np.inf,alpha = 1, epsilon= 0.05)
+    # create Monte Carlo without Exploring Starts Agent
+    # player = SARSAn(gridworld=world,n = np.inf, epsilon= 0.05,alpha = 1)
 
-# learn
-player.start(50,evaluation = True)
+    # learn
+    player.start(50,evaluation = True)
 
-# in case you want to save all the plots
-#plt.savefig("Figure_SARSA_policy_returns.png")
+    # in case you want to save all the plots you created in a picture
+    #plt.savefig("Figure_SARSA_policy_returns.png")
